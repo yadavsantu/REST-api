8c2203613922b95bd72ce4e61c8abba04912d6df
santu yadav <115726207+yadavsantu@users.noreply.github.com>
2025-09-02 14:54:20 +0545

    mongodb schema created

---
+++ models/product.js
@@ -0,0 +1,39 @@
+const mongoose=require("mongoose");
+const productSchema=new mongoose.Schema(
+    {
+    name:{
+        type:String, 
+        required:true
+    },
+
+    price:{
+        type:Number,
+        required:[true,"Price is required"]
+    },
+    featured:{
+        type:Boolean,
+        default:false
+    },
+    rating:{
+        type:Number,
+        default:4.5
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now()
+    },
+    company:{
+        type:String,
+        enum:{
+            values:['Apple','Samsung','Dell','MI'],
+            message:`{VALUE} is not supported`
+        }
+    }
+}
+);
+module.exports=mongoose.model('Product',productSchema)//collection name will be products always singular form
+
+
+
+
+
